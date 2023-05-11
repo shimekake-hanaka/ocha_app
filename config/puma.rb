74c692b9c307f28bfdd7f2d06e7866685e7c1583
shimekake-hanaka <shimekake.hanaka@icloud.com>
2023-05-11 18:31:18 +0900

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -30,7 +30,10 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+
+if Rails.env.production?
+    workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+end
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
