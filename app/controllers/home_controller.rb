41dd1e6e6851ea9e04ef2d02e163ccef41101628
s-hanaka <shimekake.hanaka@gmail.com>
2024-01-15 10:28:14 +0900

    topページの日記表示方法更新

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def top
-    @daily_posts_home = DailyPost.order(created_at: :desc).limit(3)
+    @daily_posts_home = DailyPost.where(is_open: true).order(created_at: :desc).limit(3)
   end
 
   def ocha_dailypost_show
